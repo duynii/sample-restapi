d6c6b9195e0e34332ad4ece97ee99f90fcc24492
duynii <duynii@gmail.com>
2018-12-23 17:51:13 +1100

    Completed app

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # sample-restapi
+
+## https://www.codementor.io/olatundegaruba/nodejs-restful-apis-in-10-minutes-q0sgsfhbd
+
+First start mongod by running as root 'sudo mongod --port 9117'
+
+Postman to http://localhost:3000/tasks
--- api/todoListRoutes.js
+++ api/todoListRoutes.js
@@ -1,7 +1,7 @@
-use strict';
+'use strict';
 
 module.exports = function(app) {
-  var todoList = require('../controllers/todoListController');
+  var todoList = require('./controllers/todoListController');
 
   // todoList Routes
   app.route('/tasks')
